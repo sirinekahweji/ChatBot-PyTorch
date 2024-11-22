85fb0e30a997b94fef1d1ebdf8f5c2a42a081b36
sirine kahweji <sirinekahweji5@gmail.com>
2024-11-22 20:57:28 +0100

    ajouter des data et rendre le chatbot multilanguage fr en ar

--- chatbot/chat.py
+++ chatbot/chat.py
@@ -1,5 +1,6 @@
 import random
 import json
+from langdetect import detect
 
 import torch
 
@@ -8,7 +9,7 @@ from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('intents.json', 'r') as json_data:
+with open('intents.json', 'r', encoding='utf-8', errors='ignore') as json_data:
     intents = json.load(json_data)
 
 FILE = "data.pth"
@@ -27,11 +28,19 @@ model.eval()
 
 bot_name = "FarmFinderIA"
 print("Let's chat! (type 'quit' to exit)")
+
 while True:
-    # sentence = "do you use credit cards?"
     sentence = input("You: ")
     if sentence == "quit":
         break
+    try:
+        lang = detect(sentence)
+        if lang not in ['fr', 'en', 'ar']:
+            print(f"{bot_name}: Désolé, je ne comprends que le français, l'anglais ou l'arabe.")
+            continue
+    except:
+        print(f"{bot_name}: Erreur lors de la détection de la langue.")
+        continue
 
     sentence = tokenize(sentence)
     X = bag_of_words(sentence, all_words)
@@ -50,4 +59,9 @@ while True:
             if tag == intent["tag"]:
                 print(f"{bot_name}: {random.choice(intent['responses'])}")
     else:
-        print(f"{bot_name}: I do not understand...")
+        if lang == "fr":
+            print(f"{bot_name}: Je ne comprends pas... Pouvez-vous reformuler ?")
+        elif lang == "ar":
+            print(f"{bot_name}: عذرًا، لم أفهم... هل يمكنك إعادة صياغة الجملة؟")
+        else:
+            print(f"{bot_name}: I do not understand...")
